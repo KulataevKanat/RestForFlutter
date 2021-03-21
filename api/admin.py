48ebf8e3de39b7e21002641bdd91fb36e309cc56
KulataevKanat <RaketaEnot4466@gmail.com>
2021-03-21 21:02:09 +0600

    registered new models

--- api/admin.py
+++ api/admin.py
@@ -1,5 +1,7 @@
 from django.contrib import admin
 
-from api.models import User
+from api.models import User, Category, Advert
 
 admin.site.register(User)
+admin.site.register(Category)
+admin.site.register(Advert)

5f262379d27bffd4c7dad55b757fddedfb56f7ad
KulataevKanat <RaketaEnot4466@gmail.com>
2021-03-21 21:03:32 +0600

    added DB settings

--- RestForFlutter/settings.py
+++ RestForFlutter/settings.py
@@ -105,9 +105,9 @@ WSGI_APPLICATION = 'RestForFlutter.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': '',
+        'NAME': 'RestForFlutter',
         'USER': 'postgres',
-        'PASSWORD': '',
+        'PASSWORD': 'password',
         'HOST': '127.0.0.1',
         'PORT': '5432',
 

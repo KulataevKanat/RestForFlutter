57c984f786b4b0b6ebc7674adc27fa77410d7bf7
KulataevKanat <RaketaEnot4466@gmail.com>
2021-03-21 21:03:45 +0600

    fixed

--- RestForFlutter/urls.py
+++ RestForFlutter/urls.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 from django.urls import path, include
-from swagger import urlpatterns as doc_urls
+from RestForFlutter.swagger import urlpatterns as doc_urls
 
 urlpatterns = [
     path('admin/', admin.site.urls),

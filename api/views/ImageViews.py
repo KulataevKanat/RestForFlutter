974dac4b614b717769ab494d3d0b4c4c8eb822a0
KulataevKanat <RaketaEnot4466@gmail.com>
2021-03-27 01:19:26 +0600

    Image Views

---
+++ api/views/ImageViews.py
@@ -0,0 +1,38 @@
+from rest_framework import generics
+
+from api.models import Image
+from api.serializers import ImageSerializers
+
+
+class CreateImageView(generics.CreateAPIView):
+    """Добавление изображений"""
+
+    serializer_class = ImageSerializers.AllImageSerializer
+
+
+class DeleteImageByIdView(generics.DestroyAPIView):
+    """Удаление изображений по идентификации"""
+
+    queryset = Image.objects.all()
+    serializer_class = ImageSerializers.AllImageSerializer
+
+
+class UpdateImageByIdView(generics.UpdateAPIView):
+    """Обновление изображений по идентификации"""
+
+    queryset = Image.objects.all()
+    serializer_class = ImageSerializers.AllImageSerializer
+
+
+class GetImageView(generics.ListAPIView):
+    """Вывод изображений"""
+
+    queryset = Image.objects.all()
+    serializer_class = ImageSerializers.GetImageSerializer
+
+
+class FindImageByIdView(generics.RetrieveAPIView):
+    """Поиск изображений по идентификации"""
+
+    queryset = Image.objects.all()
+    serializer_class = ImageSerializers.GetImageSerializer

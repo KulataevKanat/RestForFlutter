b3f9eee848bb60488498e8f10dc05f1b4b93515b
KulataevKanat <RaketaEnot4466@gmail.com>
2021-03-27 01:19:15 +0600

    Image Serializer

---
+++ api/serializers/ImageSerializers.py
@@ -0,0 +1,36 @@
+from rest_framework import serializers
+
+from api.models import Image
+from versatileimagefield.serializers import VersatileImageFieldSerializer
+
+
+class AllImageSerializer(serializers.ModelSerializer):
+    """Добавление, Удаление, Обновление изображений"""
+
+    class Meta:
+        model = Image
+        fields = [
+            'id',
+            'name',
+            'image'
+        ]
+
+
+class GetImageSerializer(serializers.ModelSerializer):
+    """Вывод изображений"""
+
+    image = VersatileImageFieldSerializer(
+        # sizes=[
+        #     ('full_size', 'url'),
+        #     ('thumbnail', 'thumbnail__100x100'),
+        # ]
+        sizes='product_headshot'
+    )
+
+    class Meta:
+        model = Image
+        fields = [
+            'id',
+            'name',
+            'image'
+        ]

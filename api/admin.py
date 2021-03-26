15ef54e52982e1cfbb0409f17530e1d4d9022264
KulataevKanat <RaketaEnot4466@gmail.com>
2021-03-27 01:16:55 +0600

    registered Image model

--- api/admin.py
+++ api/admin.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
 
-from api.models import User, Category, Advert
+from api.models import User, Category, Advert, Image
 
 admin.site.register(User)
+admin.site.register(Image)
 admin.site.register(Category)
 admin.site.register(Advert)

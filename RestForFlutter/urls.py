760c4dd474f0ced3b1c502eb3e78a1cf0dcc319f
KulataevKanat <RaketaEnot4466@gmail.com>
2021-03-27 01:18:15 +0600

    added DEBUG setting config

--- RestForFlutter/urls.py
+++ RestForFlutter/urls.py
@@ -1,6 +1,9 @@
 from django.contrib import admin
 from django.urls import path, include
+
+from RestForFlutter import settings
 from RestForFlutter.swagger import urlpatterns as doc_urls
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -8,3 +11,6 @@ urlpatterns = [
 ]
 
 urlpatterns += doc_urls
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)

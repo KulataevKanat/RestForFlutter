ef739b3e2d0da8f1044298f72ac7248db065fc98
KulataevKanat <RaketaEnot4466@gmail.com>
2021-05-22 08:16:53 +0600

    removed user manager

--- api/provider.py
+++ api/provider.py
@@ -1,28 +1,6 @@
 import datetime
 import jwt
 from django.conf import settings
-from django.contrib.auth.base_user import BaseUserManager
-
-
-class UserAccountManager(BaseUserManager):
-    def create_user(self, username, groups_id, email, password=None):
-        if not email:
-            raise ValueError('Email must be set!')
-        user = self.model(username=username, groups_id=groups_id, email=email)
-        user.set_password(password)
-        user.is_superuser = True
-        user.is_admin = True
-        user.is_staff = True
-        user.save(using=self._db)
-        return user
-
-    def create_superuser(self, username, groups_id, email, password):
-        user = self.create_user(username, groups_id, email, password)
-        user.save(using=self._db)
-        return user
-
-    def get_by_natural_key(self, username_):
-        return self.get(username=username_)
 
 
 def generate_access_token(user):

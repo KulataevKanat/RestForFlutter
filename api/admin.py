a21ac78552d26b72dca91b23429c763cdfde21af
KulataevKanat <RaketaEnot4466@gmail.com>
2021-05-28 03:58:27 +0600

    added publicity model

--- api/admin.py
+++ api/admin.py
@@ -1,8 +1,9 @@
 from django.contrib import admin
 
-from api.models import User, Category, Announcement, Image
+from api.models import User, Category, Announcement, Image, Publicity
 
 admin.site.register(User)
 admin.site.register(Image)
 admin.site.register(Category)
 admin.site.register(Announcement)
+admin.site.register(Publicity)

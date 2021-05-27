b2162173ae312402323043e6c766dfc54393f93b
KulataevKanat <RaketaEnot4466@gmail.com>
2021-05-28 04:01:59 +0600

    publicity serializer

---
+++ api/serializers/PublicitySerializers.py
@@ -0,0 +1,32 @@
+from rest_framework import serializers
+
+from api.models import Publicity
+from api.serializers.ImageSerializers import GetImageSerializer
+
+
+class AllPublicitySerializer(serializers.ModelSerializer):
+    """Добавление, Обновление рекламы"""
+
+    class Meta:
+        model = Publicity
+        fields = [
+            'id',
+            'image',
+            'link',
+        ]
+
+
+class GetPublicitySerializer(serializers.ModelSerializer):
+    """Вывод реклам"""
+
+    image = GetImageSerializer(many=True)
+
+    class Meta:
+        model = Publicity
+        fields = [
+            'id',
+            'image',
+            'link',
+            'created',
+            'updated',
+        ]

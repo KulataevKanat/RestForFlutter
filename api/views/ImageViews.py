df04bd72efc88cdb704a47eda123882331e5fce5
KulataevKanat <RaketaEnot4466@gmail.com>
2021-07-07 13:39:52 +0600

    added destroy image file function

--- api/views/ImageViews.py
+++ api/views/ImageViews.py
@@ -1,8 +1,12 @@
-from rest_framework import generics
+import os
 
+from rest_framework import generics, status
+from rest_framework.parsers import FormParser, MultiPartParser
+from rest_framework.response import Response
+
+from RestForFlutter import settings
 from api.models import Image
 from api.serializers import ImageSerializers
-from rest_framework.parsers import FormParser, MultiPartParser
 
 
 class CreateImageView(generics.CreateAPIView):
@@ -16,6 +20,16 @@ class DeleteImageByIdView(generics.DestroyAPIView):
     """Удаление изображений по идентификации"""
 
     queryset = Image.objects.all()
+
+    def destroy(self, request, *args, **kwargs):
+        instance = self.get_object()
+        image_root = settings.MEDIA_ROOT + "\\" + str(instance.image)
+        self.perform_destroy(instance, image_root)
+        return Response(status=status.HTTP_204_NO_CONTENT)
+
+    def perform_destroy(self, instance, image_root=None):
+        instance.delete()
+        os.remove(image_root)
 
 
 class UpdateImageByIdView(generics.UpdateAPIView):
